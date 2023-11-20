d87e026781fcb1733ff025d0f04dde040843886c
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2023-11-20 18:26:25 +0100

    refactored

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import sys
 import extruct
 import requests
 import opentelemetry.instrumentation.requests
-import pprint
 from w3lib.html import get_base_url
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 import json
 from opentelemetry import trace
+from opentelemetry.trace import Status, StatusCode
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
     ConsoleSpanExporter,
 )
+from opentelemetry.semconv.trace import SpanAttributes
 
 #sitemap_url = 'https://doi.ipk-gatersleben.de/sitemap.xml'
 sitemap_url = 'https://maps.bonares.de/finder/resources/googleds/sitemap.xml'
@@ -27,7 +27,7 @@ otel_tracer = trace.get_tracer("FAIRagro.middleware.tracer")
 opentelemetry.instrumentation.requests.RequestsInstrumentor().instrument()
 
 
-@otel_tracer.start_as_current_span("get_url")
+# @otel_tracer.start_as_current_span("get_url")
 def get_url(url):
     r = requests.get(url)
     # e!DAL returns the HTTP content-type 'text/html' which implies ISO-8859-1 encoding.
@@ -38,25 +38,22 @@ def get_url(url):
     content = r.content.decode(r.apparent_encoding)
     return content
 
-@otel_tracer.start_as_current_span("extract_sites")
+# @otel_tracer.start_as_current_span("extract_sites")
 def extract_sites(sitemap_url):
     sitemap_xml = get_url(sitemap_url)
     xml_root = ET.fromstring(sitemap_xml)
     sites = [ url.text for url in xml_root.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}loc' )]
     return sites
 
-@otel_tracer.start_as_current_span("extract_json")
-def extract_json(url):
-    html = get_url(url)
+# @otel_tracer.start_as_current_span("extract_json")
+def extract_jsonld(html):
     soup = BeautifulSoup(html, 'html.parser')
     json_ld = soup.find_all('script', type='application/ld+json')
     result = [ js.text for js in json_ld ]
     return result
     
-@otel_tracer.start_as_current_span("extract_metadata")
-def extract_metadata(url):
-    # otel_span = trace.get_current_span()
-    html = get_url(url)
+# @otel_tracer.start_as_current_span("extract_schema_org")
+def extract_schema_org_jsonld(html, url):
     base_url = get_base_url(html, url)
 
     # only scrape JSON-LD data.
@@ -67,36 +64,46 @@ def extract_metadata(url):
                                base_url=base_url,
                                uniform=True,
                                syntaxes=['json-ld'])
-    return metadata
+    return metadata['json-ld']
 
-
-def main():
-    stderr = pprint.PrettyPrinter(indent=2, stream=sys.stderr)
-
-    # sites = extract_sites(sitemap_url)
-
-    sites = [
-        "https://maps.bonares.de/finder/resources/googleds/datasets/fca18db3-fc1b-4c1c-bb81-afc0dcadc29e.html"
-        # "https://doi.org/10.5447/ipk/2012/10"
-    ]
-
-    # Extract metadata
-    result = []
-    for site in sites:
-        try:
-            metadata = extract_metadata(site)
-            result += metadata['json-ld']
-        except Exception as e:
-            stderr.pprint(site + ': ' + str(e))
-            stderr.pprint("Problematic JSON:")
-            joined_json = ''.join(extract_json(site))
-            print(joined_json, file=sys.stderr)
-
+@otel_tracer.start_as_current_span("extract_schema_org_or_issue_error")
+def extract_schema_org_or_issue_error(url):
+    otel_span = trace.get_current_span()
+    otel_span.set_attribute(SpanAttributes.URL_FULL, url)
     try:
+        content = get_url(url)
+        metadata = extract_schema_org_jsonld(content, url)
+        return metadata
+    except Exception as e:
+        suspicious_jsonld = ''.join(extract_jsonld(content))
+        otel_span.set_attribute("suspicious_jsonld", suspicious_jsonld)
+        otel_span.record_exception(e)
+        otel_span.add_event("Could not extract schema.org meta data in JSON-LD format")
+        return None
+    
+# @otel_tracer.start_as_current_span("extract_many_schema_org")    
+def extract_many_schema_org(urls):
+    for url in urls:
+        metadata = extract_schema_org_or_issue_error(url)
+        if metadata:
+            yield metadata
+
+@otel_tracer.start_as_current_span("main")   
+def main():
+    try:
+        sites = extract_sites(sitemap_url)
+
+        # sites = [
+        #     # "https://maps.bonares.de/finder/resources/googleds/datasets/fca18db3-fc1b-4c1c-bb81-afc0dcadc29e.html"
+        #     "https://doi.org/10.5447/ipk/2012/10"
+        # ]
+
+        result = list(extract_many_schema_org(sites))
         print(json.dumps(result, indent=2))
     except Exception as e:
-        stderr.pprint(str(e))
-
-
+        otel_span = trace.get_current_span()
+        otel_span.record_exception(e)
+        otel_span.set_status(Status(StatusCode.ERROR))
+        
 if __name__ == '__main__':
     main()
