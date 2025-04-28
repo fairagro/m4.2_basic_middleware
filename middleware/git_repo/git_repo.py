8492f2e4308e635d1841e63f009d611095ba51eb
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2025-04-28 11:10:53 +0000

    improved README

--- middleware/git_repo/git_repo.py
+++ middleware/git_repo/git_repo.py
@@ -169,9 +169,12 @@ class GitRepo:
                 # create initial commit
                 readme = """# Purpose of this repository #
 
-    This repository is automatically maintained by the FAIRagro middleware. It stores scraped meta
-    data from resarch data repositories in consolidated JSON-LD files.
-    """
+This repository is automatically maintained by the FAIRagro
+[middleware](https://github.com/fairagro/m4.2_basic_middleware). It stores scraped meta
+data from resarch data repositories in consolidated JSON-LD files.
+
+<mark>Important:</mark> do not change this repo manually.
+"""
                 readme_path = os.path.join(local_path, 'README.md')
                 with open(readme_path, "w", encoding="utf-8") as file:
                     file.write(readme)
