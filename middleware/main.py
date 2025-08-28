9efd33af388d2c11fdc4d9b82bcdccd0724b2b4c
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2025-08-28 09:34:14 +0000

    making 'insecure' configurable

--- middleware/main.py
+++ middleware/main.py
@@ -60,6 +60,7 @@ def setup_opentelemetry(otlp_config: dict) -> None:
     opentelemetry.instrumentation.aiohttp_client.AioHttpClientInstrumentor().instrument()
 
     endpoint = otlp_config.get("endpoint")
+    insecure = otlp_config.get("insecure", False)
     if endpoint:
         # Initialize OpenTelemetry for Tracing to OTLP endpoint
         provider = TracerProvider(
@@ -70,7 +71,7 @@ def setup_opentelemetry(otlp_config: dict) -> None:
         )
         provider.add_span_processor(
             BatchSpanProcessor(
-                OTLPSpanExporter(endpoint=endpoint, insecure=True))
+                OTLPSpanExporter(endpoint=endpoint, insecure=insecure))
         )
         trace.set_tracer_provider(provider)
     else:
