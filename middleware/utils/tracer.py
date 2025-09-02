ce17a040f593de4ce2ed3d2d243afd2dc06c244e
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2025-09-02 16:45:13 +0000

    more robust span name by simply using __qualname__ attribute

--- middleware/utils/tracer.py
+++ middleware/utils/tracer.py
@@ -13,33 +13,17 @@ def traced(func):
 
     @functools.wraps(func)
     async def async_wrapper(*args, **kwargs):
-        span_name = _get_span_name(func, args)
         tracer = trace.get_tracer(func.__module__)
-        with tracer.start_as_current_span(span_name):
+        with tracer.start_as_current_span(func.__qualname__):
             return await func(*args, **kwargs)
 
     @functools.wraps(func)
     def sync_wrapper(*args, **kwargs):
-        span_name = _get_span_name(func, args)
         tracer = trace.get_tracer(func.__module__)
-        with tracer.start_as_current_span(span_name):
+        with tracer.start_as_current_span(func.__qualname__):
             return func(*args, **kwargs)
 
     if inspect.iscoroutinefunction(func):
         return async_wrapper
     return sync_wrapper
 
-def _get_span_name(func, args):
-    """
-    creates a OTEL span name for a function
-    """
-    cls_name = ""
-    if args:
-        first = args[0]
-        # Instance method
-        if hasattr(first, "__class__") and func.__name__ in first.__class__.__dict__:
-            cls_name = first.__class__.__name__ + "."
-        # Classmethod
-        elif inspect.isclass(first) and func.__name__ in first.__dict__:
-            cls_name = first.__name__ + "."
-    return cls_name + func.__name__
